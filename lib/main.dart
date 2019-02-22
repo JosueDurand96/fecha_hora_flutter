4623ec91d5fde823c1677ce01ff846270b8f2c53
JosueDurand96 <josue.durand8@gmail.com>
2019-02-22 02:53:37 -0500

    finis

--- lib/main.dart
+++ lib/main.dart
@@ -46,23 +46,105 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(32.0),
+
+        padding: EdgeInsets.only(bottom: 50,left: 20,right: 20,top: 70),
         child: Column(
           children: <Widget>[
-            Text('Fecha seleccionada: ${_date.toString()}'),
-            RaisedButton(
-              child: Text('Seleccionar fecha'),
-              onPressed: () {
+            Container(
+              alignment: Alignment.center,
+             child: Text('Confirmar Reserva',  style: new TextStyle(
+                 fontSize: 20.0, color: Colors.black),),
+            ),
+
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(right:300.0),
+              child: Text('Desde'),
+            ),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText:'Fecha seleccionada: ${_date.toString()}',
+                border: OutlineInputBorder(),
+              ),
+              onTap: () {
                 _selectTable(context);
               },
             ),
-            Text('Hora seleccionada: ${_time.toString()}'),
-            RaisedButton(
-              child: Text('Seleccionar Hora'),
-              onPressed: () {
-                _selectTable(context);
+            SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.only(right:300.0),
+              child: Text('Hasta'),
+            ),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText:'Hora seleccionada: ${_time.toString()}',
+                border: OutlineInputBorder(),
+              ),
+              onTap: () {
+                _selectTime(context);
               },
             ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.only(right:290.0),
+              child: Text('Cancha'),
+            ),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText:'Cancha 2 - Sede Miraflores:',
+                border: OutlineInputBorder(),
+              ),
+              onTap: () {
+
+              },
+            ),
+            SizedBox(height: 40),
+            Container(
+              alignment: Alignment.center,
+              child:    Row(
+                children: <Widget>[
+                  SizedBox(width: 70.0),
+                  Container(
+                    alignment: Alignment.center,
+                    child: new Container(
+                      alignment: Alignment.center,
+                      width: 80.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                          color: Colors.yellowAccent,
+                          borderRadius: new BorderRadius.circular(0)),
+                      child: new Text(
+                        "SI",
+                        style: new TextStyle(
+                            fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 50.0),
+                  Container(
+                    alignment: Alignment.center,
+                    child: new Container(
+                      alignment: Alignment.center,
+                      width: 80.0,
+                      height: 40.0,
+                      decoration: new BoxDecoration(
+                          color: Colors.yellowAccent,
+                          borderRadius: new BorderRadius.circular(0)),
+                      child: new Text(
+                        "NO",
+                        style: new TextStyle(
+                            fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
           ],
         ),
       ),
